601ba81299cc2611a688d69dc4af0de0c31f5a47
Spenser Black <spenserblack01@gmail.com>
2026-05-29 15:10:37 -0400

    Add corruption controls
    Adds controls for corruption level, and form validation to help prevent
    the WASM from panicking.

--- wasm.go
+++ wasm.go
@@ -4,6 +4,7 @@ package main
 
 import (
 	"bytes"
+	"strconv"
 	"syscall/js"
 
 	"github.com/spenserblack/zal-go/corrupter"
@@ -16,11 +17,13 @@ func main() {
 	}
 	inputTextarea := getElementById("zalgo-input")
 	outputEl := getElementById("zalgo-output")
+	minCorruption := must(intValue(getElementById("minimum-corruption")))
+	maxCorruption := must(intValue(getElementById("maximum-corruption")))
 
 	buf := new(bytes.Buffer)
 	corrupter := corrupter.New(buf)
-	corrupter.Min = 5
-	corrupter.Max = 10
+	corrupter.Min = minCorruption
+	corrupter.Max = maxCorruption
 
 	rawText := inputTextarea.Get("value").String()
 	for _, r := range rawText {
@@ -28,4 +31,16 @@ func main() {
 	}
 	corruptedBytes := buf.Bytes()
 	outputEl.Set("textContent", string(corruptedBytes))
+}
+
+func intValue(el js.Value) (int, error) {
+	value := el.Get("value").String()
+	return strconv.Atoi(value)
+}
+
+func must[T any](value T, err error) T {
+	if err != nil {
+		panic(err)
+	}
+	return value
 }
